9b9de51b18582c285a6335b248ae01a0e6bec06d
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-11-04 17:47:42 +0530

    Added Basic Structure for the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import FooterComponent from './components/FooterComponent'
+import MainComponent from './components/MainComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <MainComponent />
+      <FooterComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <footer class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="https://pankajmalviya.netlify.app/">Pankaj Malviya</a>.
+        </footer>
+    )
+}
+
+export default FooterComponent
