2c1ef5f3d40f5f5eb24cdb96b3787160267965fe
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-11-04 13:27:19 +0530

    Added Scss variables

---
+++ src/components/FooterFrontendMentor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FooterFrontendMentor = ({ name }) => {
+    return (
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="https://pankajmalviya.netlify.app/">{name}</a>.
+        </div>
+    )
+}
+
+export default FooterFrontendMentor
---
+++ src/components/ProductPreviewCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductPreviewCard = () => {
+    return (
+        <div className="card-container">
+            
+        </div>
+    )
+}
+
+export default ProductPreviewCard
