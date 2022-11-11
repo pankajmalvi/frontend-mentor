ada9143b43f3a0c08df6c45c206be5ad212d6384
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-11-11 16:12:51 +0530

    Final Touches

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="attribution">
-            <p class="attribution">
+            <p className="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
                 Coded by <a href="https://pankajmalviya.netlify.app/" target="_blank">Pankaj Malviya</a>.
             </p>
