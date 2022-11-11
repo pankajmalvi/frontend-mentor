f1593c9dc28a3270404ccf331df804c95e608740
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-11-11 10:28:22 +0530

    Header Structure updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import IconLogo from './IconLogo'
 
 const Header = () => {
     return (
-        <header>
-            Home
-            New
-            Popular
-            Trending
-            Categories
+        <header className='main-header'>
+            <div className="brand-logo">
+                <IconLogo />
+            </div>
+            <nav className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>New</li>
+                    <li>Popular</li>
+                    <li>Trending</li>
+                    <li>Categories</li>
+                </ul>
+            </nav>
         </header>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,35 +3,64 @@ import React from 'react'
 const Main = () => {
     return (
         <main>
-            The Bright Future of Web 3.0?
+            <div className="news-heading-image">
 
-            We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
-            But is it really fulfilling its promise?
+            </div>
+            <div className="heading-news-content">
 
-            Read more
+                <h1 className="news-heading">
+                    The Bright Future of Web 3.0?
+                </h1>
+                <div className="news-content">
+                    <p className="head-news-description">
+                        We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+                        But is it really fulfilling its promise?
+                    </p>
+                    <button className="read-more-button">
+                        Read more
+                    </button>
+                </div>
+            </div>
+            <div className="new-news-container">
+                <header className="new-news-header">
+                    New
+                </header>
 
-            New
+                <div className="new-news-items">
+                    <div className="new-item">
+                        Hydrogen VS Electric Cars
+                        Will hydrogen-fueled cars ever catch up to EVs?
+                    </div>
+                    <div className="new-item">
+                        The Downsides of AI Artistry
+                        What are the possible adverse effects of on-demand AI image generation?
+                    </div>
+                    <div className="new-item">
+                        Is VC Funding Drying Up?
+                        Private funding by VC firms is down 50% YOY. We take a look at what that means.
+                    </div>
+                </div>
+            </div>
 
-            Hydrogen VS Electric Cars
-            Will hydrogen-fueled cars ever catch up to EVs?
+            <div className="popular-news-items">
+                <div className="popular-news-item">
+                    01
+                    Reviving Retro PCs
+                    What happens when old PCs are given modern upgrades?
+                </div>
 
-            The Downsides of AI Artistry
-            What are the possible adverse effects of on-demand AI image generation?
+                <div className="popular-news-item">
+                    02
+                    Top 10 Laptops of 2022
+                    Our best picks for various needs and budgets.
+                </div>
 
-            Is VC Funding Drying Up?
-            Private funding by VC firms is down 50% YOY. We take a look at what that means.
-
-            01
-            Reviving Retro PCs
-            What happens when old PCs are given modern upgrades?
-
-            02
-            Top 10 Laptops of 2022
-            Our best picks for various needs and budgets.
-
-            03
-            The Growth of Gaming
-            How the pandemic has sparked fresh opportunities.
+                <div className="popular-news-item">
+                    03
+                    The Growth of Gaming
+                    How the pandemic has sparked fresh opportunities.
+                </div>
+            </div>
 
         </main>
     )
