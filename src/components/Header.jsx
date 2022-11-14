c01c53b8e59808e138ecbf32340f9d58b2330417
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-11-14 12:41:03 +0530

    Added Sass variables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,23 @@ import IconLogo from './IconLogo'
 const Header = () => {
     return (
         <header>
-            <div className="brand-logo">
-                <IconLogo />
-            </div>
-            <nav className='nav-links'>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Careers</a></li>
-                </ul>
-            </nav>
-            <button className='nav-btn'>Request Invite</button>
-            <nav>
+            <div className="navbar">
 
-            </nav>
+                <div className="brand-logo">
+                    <IconLogo />
+                </div>
+                <nav className='nav-links'>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                </nav>
+                <button className='nav-btn'>Request Invite</button>
+            </div>
+
         </header>
     )
 }
